9451ff918f4e7a1a60d9fc353a4f8f74a0ef3fc3
Tanawit Pattanaveerangkoon <tanawit.pat@gmail.com>
2020-07-12 16:05:47 +0700

    Create post pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,23 @@
+import Link from "next/link";
+
+const PostLink = ({ id, title }: { id: String; title: String }) => (
+  <Link href="/posts/[id]" as={`/posts/${id}`}>
+    <a>{title}</a>
+  </Link>
+);
+
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <ul>
+      <li>
+        <PostLink id="1" title="Post One" />
+      </li>
+      <li>
+        <PostLink id="2" title="Post Two" />
+      </li>
+      <li>
+        <PostLink id="3" title="Post Three" />
+      </li>
+    </ul>
+  );
 }
---
+++ pages/posts/[id].tsx
@@ -0,0 +1,58 @@
+import { GetStaticPaths, GetStaticProps } from "next";
+import Link from "next/link";
+
+const revalidateInterval = 60;
+
+export default function PostPage({
+  id,
+  title,
+  author,
+  updatedAt,
+}: {
+  id: String;
+  title: String;
+  author: String;
+  updatedAt: Date;
+}) {
+  const timeString = new Date(updatedAt).toLocaleTimeString();
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>Post ID: {id}</p>
+      <p>Author: {author}</p>
+      <p>
+        Updated at <span>{timeString}</span>
+      </p>
+      <div>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [{ params: { id: "1" } }, { params: { id: "2" } }],
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { id } = params;
+
+  const response = await fetch(`http://localhost:4000/posts/${id}`);
+  const { title, author } = await response.json();
+
+  return {
+    props: {
+      id,
+      title,
+      author,
+      updatedAt: Date.now(),
+    },
+    unstable_revalidate: revalidateInterval,
+  };
+};
